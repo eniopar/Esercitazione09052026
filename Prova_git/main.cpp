4c27406a3295f7a87f0d4ef8b5285b3be4cfe83f
enio <eniopar.ep@gmail.com>
2026-05-09 11:22:58 +0200

    aggiunta funzione raddoppia e modificato .gitignore

--- Prova_git/main.cpp
+++ Prova_git/main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include "salatti.h"
 using namespace std;
 
 int main(int argc, char** argv) {
 	
 	cout<<"ciao a tutti"<<endl;
+	int numeroDoppio=raddoppia(5);
+	cout<<numeroDoppio;
+	
 	
 	return 0;
 }
---
+++ Prova_git/salatti.h
@@ -0,0 +1,8 @@
+#ifndef SALATTI_H
+#define SALATTI_H
+
+int raddoppia(int numero){
+	return numero*2;
+}
+
+#endif
