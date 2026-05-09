86d2b1c38552cf49d166a60e19ccd30f417b94e7
enio <eniopar.ep@gmail.com>
2026-05-09 11:29:33 +0200

    aggiunta funzione triplica

---
+++ Prova_git/Turini.h
@@ -0,0 +1,6 @@
+#ifndef TURINI_H
+#define TURINI_H
+ int triplica (int n){
+	return n*3;
+ }
+#endif
--- Prova_git/main.cpp
+++ Prova_git/main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
+#include "Turini.h"
 using namespace std;
 
 int main(int argc, char** argv) {
 	
 	cout<<"ciao a tutti"<<endl;
-	
+	int n = triplica(67);
+	cout<<n<<endl;
 	return 0;
 }
-
